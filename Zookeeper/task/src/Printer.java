c2cdb9db0e2a734591e6ad72c31ce21e38cf44a7
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2022-03-24 11:12:36 +0200

    Rush into print

---
+++ Zookeeper/task/src/Printer.java
@@ -0,0 +1,10 @@
+public class Printer {
+    static void print() {
+        System.out.println("""
+                I love animals!
+                Let's check on the animals...
+                The deer looks fine.
+                The bat looks happy.
+                The lion looks healthy.""");
+    }
+}
